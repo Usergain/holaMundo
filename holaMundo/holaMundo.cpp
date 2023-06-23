302a6f2007badda77ddb99917dad876fe7423683
Usergain <argaz.sl@gmail.com>
2023-06-23 17:30:31 +0200

    commit inicial de pruebas con commit

---
+++ holaMundo/holaMundo.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    string nombre;
+    cout << nombre.length()<<endl;
+    for (int i = 0; i < 10; i++)
+    {
+        cout << i << endl;
+    }
+    return 0;
+}
